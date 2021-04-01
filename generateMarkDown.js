e1fe28e2ba2db4b9be0bdbbc9565415ae3f4c898
saranya-code <saranyadayalan28@gmail.com>
2021-04-01 17:27:31 -0500

    Final README

--- generateMarkDown.js
+++ generateMarkDown.js
@@ -9,7 +9,7 @@ const createdMarkDown = `# ${answer.title}\n
 ![javascript](https://img.shields.io/badge/Javascript-100%25-yellowgreen)
 ![node](https://img.shields.io/badge/npm-100%25-orange)
 ## Description\n
-    ${answer.description} \n
+${answer.description} \n
 ## Table of content\n
 1. [Installation](#installation) 
 2. [Usage](#usage) 
@@ -31,8 +31,8 @@ const createdMarkDown = `# ${answer.title}\n
 ### Tests \n
     ${answer.test}\n 
 ### Questions \n      
-### If you have any questions feel free to contact me [${answer.gitHubUserName}](https://github.com/${answer.gitHubUserName}) on my Email ${answer.gitHubId} \n 
-### You can also look my [Repository](https://github.com/${answer.gitHubUserName}/${answer.gitHubRepositoryName}) \n
+* If you have any questions feel free to contact me [${answer.gitHubUserName}](https://github.com/${answer.gitHubUserName}) or Email me ${answer.gitHubId} \n 
+* You can also look my [Repository](https://github.com/${answer.gitHubUserName}/${answer.gitHubRepositoryName}) \n
 ### Screenshot \n
 ![Webpage Screenshot ](./screenshot/readme_questions.png?raw=true)`;
 
